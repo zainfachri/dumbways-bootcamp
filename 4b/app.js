4994b7d1d213ddc20ed77131f86c10bf3d905ec5
zainfachri <zainfachri23@gmail.com>
2020-06-20 18:21:53 +0700

    update file

--- 4b/app.js
+++ 4b/app.js
@@ -30,4 +30,62 @@ connection.connect((err) => {
     );
   });
   
+  app.get('/stock',(req, res)=> {
+    connection.query(
+        'SELECT * FROM game',
+        (error, results) => {
+            console.log(results);
+            res.render('stock.ejs', {game: results});
+        }
+    );
+});
+
+  app.get('/genre', (req, res) => {
+    res.render('genre.ejs');
+  });
+
+  app.get('/game', (req, res) => {
+    connection.query(
+        'SELECT * FROM genre',
+        (error, results) => {
+            console.log(results);
+            res.render('game.ejs', {genre: results});
+        }
+    );
+});
+app.post('/create', (req, res) => {
+    connection.query(
+      'INSERT INTO game (title, image, genre_id) VALUES (?, ?, ?)',
+      [req.body.gameTitle, req.body.gameImg, req.body.gameGenre],
+      (error, results) => {
+        res.redirect('/');
+      }
+    );
+  });
+app.post('/createStock', (req, res) => {
+    connection.query(
+      'INSERT INTO transaction (price, game_id, stock) VALUES (?, ?, ?)',
+      [req.body.gamePrice, req.body.gameId, req.body.gameStock],
+      (error, results) => {
+        res.redirect('/');
+      }
+    );
+});
+app.post('/createGenre', (req, res) => {
+    connection.query(
+      'INSERT INTO genre (name) VALUES (?)',
+      [req.body.gameGenreAdd],
+      (error, results) => {
+        res.redirect('/');
+      }
+    );
+});
+app.post('/update/:id', (req, res) => {
+    connection.query(
+    'UPDATE transaction SET stock = ?  WHERE id = ?',[req.body.buy, req.params.id],
+    (error, results)=>{
+      res.redirect('/');
+    })
+  });
+
   app.listen(3000);
